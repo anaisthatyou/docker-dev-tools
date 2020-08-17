222f157dac5270157c384255527d549eb64ff210
Ana Harrison <ana@harrison.io>
2020-08-17 15:01:31 +0100

    Update script to add add to trust store automatically.

--- make_certs.sh
+++ make_certs.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 cwd=$(pwd)
-certs_dir="${cwd}/certs"
+certs_dir=$cwd/certs
 
-if [ -d "$certs_dir" ]; then
+if [ -d $certs_dir ]; then
   echo "${certs_dir} already exists. Remove or use exists certs.";
   exit 1;
 fi
 
-mkdir certs
-mkdir certs/portainer
-mkdir certs/pgadmin
+mkdir $certs_dir
+mkdir $certs_dir/portainer
+mkdir $certs_dir/pgadmin
 
-mkcert -cert-file certs/portainer/cert.pem -key-file certs/portainer/key.pem portainer.dev
-mkcert -cert-file certs/pgadmin/cert.pem -key-file certs/pgadmin/key.pem pgadmin.dev
+mkcert -cert-file $certs_dir/portainer/cert.pem -key-file $certs_dir/portainer/key.pem portainer.dev
+mkcert -cert-file $certs_dir/pgadmin/cert.pem -key-file $certs_dir/pgadmin/key.pem pgadmin.dev
 
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain certs/portainer/cert.pem certs/pgadmin/cert.pem
+echo ${certs_dir}/pgadmin/cert.pem
+
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/pgadmin/cert.pem
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/portainer/cert.pem 
