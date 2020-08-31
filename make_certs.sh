c1ee7f00b5860327e2eab9ffb3c8f7b480bbb9b7
Ana Harrison <ana@harrison.io>
2020-08-31 20:15:45 +0100

    Added json-server to docker dev tools.

--- make_certs.sh
+++ make_certs.sh
@@ -10,11 +10,12 @@ fi
 mkdir $certs_dir
 mkdir $certs_dir/portainer
 mkdir $certs_dir/pgadmin
+mkdir $certs_dir/json-server
 
 mkcert -cert-file $certs_dir/portainer/cert.pem -key-file $certs_dir/portainer/key.pem portainer.dev
 mkcert -cert-file $certs_dir/pgadmin/cert.pem -key-file $certs_dir/pgadmin/key.pem pgadmin.dev
-
-echo ${certs_dir}/pgadmin/cert.pem
+mkcert -cert-file $certs_dir/json-server/cert.pem -key-file $certs_dir/json-server/key.pem json-server.dev
 
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/pgadmin/cert.pem
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/portainer/cert.pem 
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/json-server/cert.pem 
