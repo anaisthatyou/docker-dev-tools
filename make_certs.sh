0890fea3f03ce965adeb1f95343e1509c1d2bf25
Ana Harrison <ana@harrison.io>
2021-02-08 12:27:37 +0000

    Remove networks, updated script mock name

--- make_certs.sh
+++ make_certs.sh
@@ -10,12 +10,12 @@ fi
 mkdir $certs_dir
 mkdir $certs_dir/portainer
 mkdir $certs_dir/pgadmin
-mkdir $certs_dir/json-server
+mkdir $certs_dir/mock
 
 mkcert -cert-file $certs_dir/portainer/cert.pem -key-file $certs_dir/portainer/key.pem portainer.dev
 mkcert -cert-file $certs_dir/pgadmin/cert.pem -key-file $certs_dir/pgadmin/key.pem pgadmin.dev
-mkcert -cert-file $certs_dir/json-server/cert.pem -key-file $certs_dir/json-server/key.pem json-server.dev
+mkcert -cert-file $certs_dir/mock/cert.pem -key-file $certs_dir/mock/key.pem mock.dev
 
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/pgadmin/cert.pem
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/portainer/cert.pem 
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/json-server/cert.pem 
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/mock/cert.pem 
