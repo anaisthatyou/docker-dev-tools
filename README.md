47270ebc1b90c3008da34212383ca60bc550f960
Ana Harrison <ana@harrison.io>
2021-03-08 23:08:39 +0000

    Added LDAP, LDAP Admin and a Debugger image.
    Added LDAP for Authentication for the other services. Created an image
    which I can use to debug other containers on the network.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Personal docker-compose stacks for some local dev tools and shared containers
 
 Shared services that can be used across many projects.
 
-[PostgreSQL]()
+[PostgreSQL](https://www.postgresql.org/)
 
 ## dev-tools
 
@@ -17,12 +17,17 @@ Shared services that can be used across many projects.
 3. Add following lines to `private/etc/hosts`.
 4. Run `docker-compose up -d`.
 
-```
-127.0.0.1 pgadmin.dev
-127.0.0.1 portainer.dev
-127.0.0.1 mock.dev
+```hosts
+127.0.0.1       pgadmin.dev
+127.0.0.1       portainer.dev
+127.0.0.1       mock.dev
+127.0.0.1       ldap-admin.dev
 ```
 
 [pgadmin](https://www.pgadmin.org/) available at <https://pgadmin.dev>
 
 [portainer](https://www.portainer.io/) available at <https://portainer.dev>
+
+[ldap]
+
+[ldap-user-manager](https://github.com/wheelybird/ldap-user-manager) available at <https://ldap-admin.dev>
--- dev-tools/make_certs.sh
+++ dev-tools/make_certs.sh
@@ -11,11 +11,10 @@ mkdir $certs_dir
 mkdir $certs_dir/portainer
 mkdir $certs_dir/pgadmin
 mkdir $certs_dir/mock
+mkdir $certs_dir/ldap
 
 mkcert -cert-file $certs_dir/portainer/cert.pem -key-file $certs_dir/portainer/key.pem ana-macbook.portainer portainer.dev 
 mkcert -cert-file $certs_dir/pgadmin/cert.pem -key-file $certs_dir/pgadmin/key.pem ana-macbook.pgadmin pgadmin.dev
 mkcert -cert-file $certs_dir/mock/cert.pem -key-file $certs_dir/mock/key.pem ana-macbook.mock mock.dev
+mkcert -cert-file $certs_dir/ldap/cert.pem -key-file $certs_dir/ldap/key.pem ana-macbook.ldap ldap.dev
 
-# sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/pgadmin/cert.pem
-# sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/portainer/cert.pem 
-# sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $certs_dir/mock/cert.pem 
