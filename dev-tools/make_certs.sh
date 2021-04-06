8b121c43c205d9453777412a726bc061f04fcdfe
Ana Harrison <ana@harrison.io>
2021-04-06 10:16:05 +0100

    Updates LDAP root. Fixes admin binding account cn
    Updates LDAP to build correctly and and use the inital admin user correctly with the admin image. Prettier formatting also ran updating the quoation formating.

--- dev-tools/make_certs.sh
+++ dev-tools/make_certs.sh
@@ -11,10 +11,10 @@ mkdir $certs_dir
 mkdir $certs_dir/portainer
 mkdir $certs_dir/pgadmin
 mkdir $certs_dir/mock
-mkdir $certs_dir/ldap
+mkdir $certs_dir/ldap-admin
 
 mkcert -cert-file $certs_dir/portainer/cert.pem -key-file $certs_dir/portainer/key.pem ana-macbook.portainer portainer.dev 
 mkcert -cert-file $certs_dir/pgadmin/cert.pem -key-file $certs_dir/pgadmin/key.pem ana-macbook.pgadmin pgadmin.dev
 mkcert -cert-file $certs_dir/mock/cert.pem -key-file $certs_dir/mock/key.pem ana-macbook.mock mock.dev
-mkcert -cert-file $certs_dir/ldap/cert.pem -key-file $certs_dir/ldap/key.pem ana-macbook.ldap ldap.dev
+mkcert -cert-file $certs_dir/ldap-admin/cert.pem -key-file $certs_dir/ldap-admin/key.pem ana-macbook.ldap-admin ldap-admin.dev
 
